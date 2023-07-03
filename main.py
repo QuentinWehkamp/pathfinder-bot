d3c143522ce7b89a357915d8dacde7104f809f69
QuentinWehkamp <quentinwehkamp@gmail.com>
2023-07-03 14:15:05 +0200

    "

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uservariables
 from os import path
 import requests
-from requests.adapters import HTTPAdapter, Retry
+from time import sleep
 
 TOKEN = uservariables.botToken
 chat_id = uservariables.chatID
@@ -10,20 +10,18 @@ message0 = "Hard Drive is down"
 message1 = "Hard Drive is up"
 check = None
 sent = None
-retries = Retry(total=5,
-                backoff_factor=0.1,
-                status_forcelist=[ 500, 502, 503, 504 ])
 
 while True:
+    sleep(2)
     if not path.exists(find):
         check = 0
         if not sent == 0:
             url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message0}"
-            print(requests.get(url).json(), HTTPAdapter(max_retries=retries))  # this sends the message
+            print(requests.get(url).json())  # this sends the message
             sent = 0
     elif path.exists(find):
         check = 1
         if not sent == 1:
             url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message1}"
-            print(requests.get(url).json(), HTTPAdapter(max_retries=retries))  # this sends the message
+            print(requests.get(url).json())  # this sends the message
             sent = 1
